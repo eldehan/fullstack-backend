e90b323a64cd8cecfcd08637af3390ea24fd124c
Hans Elde <hanserik.elde@gmail.com>
2022-07-15 10:20:35 -0700

    backend prepped for deploy

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+morgan.token('postdata', (request, response) => {
+  return JSON.stringify(request.body)
+})
+
+app.use(cors())
 app.use(express.json())
+app.use(morgan(':method :url :response-time :postdata'))
 
 app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p>
@@ -63,7 +71,7 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
