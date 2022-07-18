c48f0919d94af1ee5f0705a87db7c82b2748a36c
Hans Elde <hanserik.elde@gmail.com>
2022-07-17 17:45:30 -0700

    Completed part 3c; phonebook entirely configured to use database

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
 
+const morgan = require('morgan')
 morgan.token('postdata', (request, response) => {
   return JSON.stringify(request.body)
 })
@@ -13,37 +15,36 @@ app.use(cors())
 app.use(express.static('build'))
 
 app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p>
-  
-  <p>${new Date().toString()}</p>`)
+  Person.find({}).then(people => {
+    let JSONpeople = people.map(person => {
+      return JSON.parse(JSON.stringify(person))
+    })
+
+    response.send(`<p>Phonebook has info for ${JSONpeople.length} people</p>
+    
+    <p>${new Date().toString()}</p>`)
+  })
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.statusMessage = 'No such person located in database'
-    response.status(404).end()
-  }
+  })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
-const generateId = () => {
-  return Math.ceil(Math.random() * 100000)
-}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -54,48 +55,50 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId()
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
---
+++ mongo.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+
+let newName
+let newNumber
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+} 
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@fullstack-backend.qxoct.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length > 3) {
+  newName = process.argv[3]
+  newNumber = process.argv[4]
+
+  mongoose.connect(url)
+  .then((result) => {
+    console.log('connected')
+
+    if (newName && newNumber) {
+      const newPerson = new Person({
+        name: newName,
+        number: newNumber
+      })
+
+      return newPerson.save()
+    }
+  })
+  .then(() => {
+    console.log('operation complete')
+    return mongoose.connection.close()
+  })
+  .catch(err => console.log(err))
+} else {
+  mongoose.connect(url)
+    .then((result) => {
+      console.log('connected')
+
+      Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(person)
+        })
+        mongoose.connection.close()
+      })
+    })
+}
